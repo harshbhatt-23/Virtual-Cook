a2d0a5eebb70641c8f7a7c76992c742e8b3e6a52
harsh bhatt <bhr.2306@gmail.com>
2023-06-05 23:39:24 -0400

    [#11] Implementation of Multi language support
    Added Multi language support to the App.

--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -1,13 +1,57 @@
-import { Button } from "react-native-paper";
-import React from "react";
+import { Button, Switch } from "react-native-paper";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import style from "./styles";
+import "../../i18n";
+import { useTranslation } from "react-i18next";
 
 export default function SettingsScreen({ navigation }) {
+  const { t, i18n } = useTranslation();
+
+  const [currentLanguage, setLanguage] = useState("en");
+
+  const changeLanguage = (value) => {
+    i18n
+      .changeLanguage(value)
+      .then(() => setLanguage(value))
+      .catch((err) => console.log(err));
+  };
+
+  const [isSwitchOn, setIsSwitchOn] = React.useState(false);
+  const onToggleSwitch = () => {
+    setIsSwitchOn(!isSwitchOn);
+    if (currentLanguage === "en") {
+      changeLanguage("fr");
+      //saveLanguage("fr");
+    } else {
+      changeLanguage("en");
+      //saveLanguage("en");
+    }
+  };
+
+  // const saveLanguage = async (selectedLanguage) => {
+  //   try {
+  //     await AsyncStorage.setItem("appLanguage", selectedLanguage);
+  //     console.log("Language saved successfully");
+  //     Updates.reloadAsync();
+  //   } catch (error) {
+  //     console.log("Error saving language:", error);
+  //   }
+  // };
+
   return (
     <View style={style.container}>
-      <Text>Settings Screen</Text>
-      <Button mode="contained">This is Settings Page</Button>
+      <Button mode="contained">{t("this line is translated")}</Button>
+
+      <View style={style.switch}>
+        <Text>
+          {currentLanguage === "en"
+            ? t("change language")
+            : t("change language")}
+        </Text>
+
+        <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />
+      </View>
     </View>
   );
 }
