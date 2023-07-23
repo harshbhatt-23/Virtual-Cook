49d117ec3ba089919bcbbc3d27a6e3762605fc6e
milindkate <123139620+milindkate@users.noreply.github.com>
2023-07-23 19:02:59 -0400

    Home screen layout added in repository

--- src/components/homescreen/Home.js
+++ src/components/homescreen/Home.js
@@ -1,12 +1,81 @@
-import { Button } from "react-native-paper";
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import style from "./styles";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import {
+  IconButton,
+  MD3Colors,
+  Divider,
+  Button,
+  useTheme,
+  Text,
+} from "react-native-paper";
+import { recipes } from "../../components/data/RecipeData";
+import styles from "./styles";
+import { connect } from "react-redux";
 
-export default function Home({ navigation }) {
+const Home = ({ navigation, language }) => {
+  const [recipeList, setRecipeList] = useState([]);
+  const theme = useTheme();
+
+  useEffect(() => {
+    setRecipeList(recipes);
+  }, []);
+
+  const menuLabels = {
+    en: {
+      helloUser: "Hello, User",
+      descriptionText:"What would you like to cook today?",
+      recipeText:"Popular recipes"
+    },
+    fr: {
+      helloUser: "Bonjour, utilisateur",
+      descriptionText:"Qu'aimeriez-vous cuisiner aujourd'hui?",
+      recipeText:"Recettes populaires"
+    },
+  };
+
+  const renderRecipeItem = ({ item }) => (
+    <TouchableOpacity style={styles.recipeItemContainer}>
+      <Image source={{ uri: item.photo_url }} style={styles.recipeImage} />
+      <Text style={styles.recipeTitle}>{item.title[language]}</Text>
+    </TouchableOpacity>
+  );
+
   return (
-    <View style={style.container}>
-      <Text>Home Screen</Text>
+    <View style={[styles.container, { backgroundColor: theme.colors.surface }]}>
+      <View style={styles.header}>
+        <View style={styles.headerLeft}>
+          <Text style={styles.headerText}>{menuLabels[language].helloUser}</Text>
+          <Text style={styles.description}>
+          {menuLabels[language].descriptionText}
+          </Text>
+        </View>
+        <View style={styles.headerRight}>
+          <IconButton icon="account-circle" color={MD3Colors.primary50}
+          size={50} />
+        </View>
+      </View>
+      <Divider />
+      <View style={styles.recipeHeadingView}>
+        <Text style={styles.recipeHeadingText}>{menuLabels[language].recipeText}</Text>
+      </View>
+      <FlatList
+        data={recipeList}
+        renderItem={renderRecipeItem}
+        keyExtractor={(item) => `${item.recipeId}`}
+      />
     </View>
   );
-}
+};
+
+const mapStateToProps = (state) => ({
+  language: state.language,
+  veg: state.veg,
+});
+
+export default connect(mapStateToProps)(Home);
--- src/components/homescreen/styles.js
+++ src/components/homescreen/styles.js
@@ -3,9 +3,62 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
   },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 16,
+    marginTop: 50,
+  },
+  headerLeft: {
+    flex: 1,
+  },
+  headerRight: {},
+  headerText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  description: {
+    fontSize: 26,
+    marginTop: 8,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginVertical: 20,
+    alignSelf: 'center',
+  },
+  recipeItemContainer: {
+    margin:10,
+    marginBottom: 20,
+    borderRadius: 10,
+    overflow: 'hidden',
+    alignItems: 'center',
+    borderColor: "#cccccc",
+    borderWidth: 0.5,
+  },
+  recipeImage: {
+    width: '100%',
+    height: 200,
+    borderRadius: 10,
+  },
+  recipeTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 8,
+    padding: 8,
+    textAlign: 'center',
+  },
+  recipeHeadingView:{
+    
+  },
+  recipeHeadingText:{
+    fontSize: 18,
+    marginTop: 10,
+    marginBottom: 8,
+    paddingLeft: 12,
+  }
 });
 
 export default styles;
